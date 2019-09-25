53a38be2bc939cef0887eef5580e3ebff5040455
100102324 <hopdeww@gmail.co>
2019-09-25 09:04:55 -0500

    Initial commit

---
+++ Life.py
@@ -0,0 +1,21 @@
+import turtle
+scn = turtle.Screen()
+k = turtle.Turtle()
+k.shape("turtle")
+k.turtlesize(.5)
+turtle.bgcolor("slate gray")
+turtle.forward(5)
+
+k.penup()
+k.color("Gray")
+k.goto()
+
+
+
+
+
+
+
+
+turtle.exitonclick()
+
